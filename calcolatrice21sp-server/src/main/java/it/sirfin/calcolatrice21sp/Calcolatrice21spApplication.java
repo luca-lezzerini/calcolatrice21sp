e79c9dba8bc721f5238f644a9f7f80a560a90abb
luca-lezzerini <47611629+luca-lezzerini@users.noreply.github.com>
2021-02-05 10:05:35 +0100

    Primo commit del server

---
+++ calcolatrice21sp-server/src/main/java/it/sirfin/calcolatrice21sp/Calcolatrice21spApplication.java
@@ -0,0 +1,13 @@
+package it.sirfin.calcolatrice21sp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Calcolatrice21spApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Calcolatrice21spApplication.class, args);
+	}
+
+}
---
+++ calcolatrice21sp-server/src/test/java/it/sirfin/calcolatrice21sp/Calcolatrice21spApplicationTests.java
@@ -0,0 +1,13 @@
+package it.sirfin.calcolatrice21sp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Calcolatrice21spApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
