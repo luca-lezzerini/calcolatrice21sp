e71b89b474cf87ca2ae518b2ffb114b9bdc1335a
luca-lezzerini <47611629+luca-lezzerini@users.noreply.github.com>
2021-02-05 11:02:56 +0100

    Creati servizi web nel controller

--- calcolatrice21sp-server/src/main/java/it/sirfin/calcolatrice21sp/controller/CalcolatriceController.java
+++ calcolatrice21sp-server/src/main/java/it/sirfin/calcolatrice21sp/controller/CalcolatriceController.java
@@ -1,6 +1,11 @@
 package it.sirfin.calcolatrice21sp.controller;
 
+import it.sirfin.calcolatrice21sp.dto.CalcolatriceReqDto;
+import it.sirfin.calcolatrice21sp.dto.CalcolatriceResDto;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -10,7 +15,50 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin("*")
 @RestController
 public class CalcolatriceController {
-    
-    
-    
+
+    @RequestMapping("/somma")
+    @ResponseBody
+    public CalcolatriceResDto somma(@RequestBody CalcolatriceReqDto dto) {
+        double r = dto.getOp1() + dto.getOp2();
+        CalcolatriceResDto risp = new CalcolatriceResDto();
+        risp.setRisultato(r);
+        return risp;
+    }
+
+    @RequestMapping("/moltiplica")
+    @ResponseBody
+    public CalcolatriceResDto moltiplica(@RequestBody CalcolatriceReqDto dto) {
+        double r = dto.getOp1() * dto.getOp2();
+        CalcolatriceResDto risp = new CalcolatriceResDto();
+        risp.setRisultato(r);
+        return risp;
+    }
+
+    @RequestMapping("/dividi")
+    @ResponseBody
+    public CalcolatriceResDto dividi(@RequestBody CalcolatriceReqDto dto) {
+        double r = dto.getOp1() / dto.getOp2();
+        CalcolatriceResDto risp = new CalcolatriceResDto();
+        risp.setRisultato(r);
+        return risp;
+    }
+
+    @RequestMapping("/sottrai")
+    @ResponseBody
+    public CalcolatriceResDto sottrai(@RequestBody CalcolatriceReqDto dto) {
+        double r = dto.getOp1() - dto.getOp2();
+        CalcolatriceResDto risp = new CalcolatriceResDto();
+        risp.setRisultato(r);
+        return risp;
+    }
+
+    @RequestMapping("/modulo")
+    @ResponseBody
+    public CalcolatriceResDto modulo(@RequestBody CalcolatriceReqDto dto) {
+        double r = dto.getOp1() % dto.getOp2();
+        CalcolatriceResDto risp = new CalcolatriceResDto();
+        risp.setRisultato(r);
+        return risp;
+    }
+
 }
