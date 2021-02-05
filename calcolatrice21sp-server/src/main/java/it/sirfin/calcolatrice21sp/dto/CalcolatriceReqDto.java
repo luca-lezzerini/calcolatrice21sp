50fc91ebbba75994d79e46c59583a06a9d43b645
luca-lezzerini <47611629+luca-lezzerini@users.noreply.github.com>
2021-02-05 10:40:21 +0100

    Creati DTO

---
+++ calcolatrice21sp-server/src/main/java/it/sirfin/calcolatrice21sp/dto/CalcolatriceReqDto.java
@@ -0,0 +1,27 @@
+package it.sirfin.calcolatrice21sp.dto;
+
+public class CalcolatriceReqDto {
+
+    private double op1;
+    private double op2;
+
+    public CalcolatriceReqDto() {
+    }
+
+    public double getOp1() {
+        return op1;
+    }
+
+    public void setOp1(double op1) {
+        this.op1 = op1;
+    }
+
+    public double getOp2() {
+        return op2;
+    }
+
+    public void setOp2(double op2) {
+        this.op2 = op2;
+    }
+
+}
---
+++ calcolatrice21sp-server/src/main/java/it/sirfin/calcolatrice21sp/dto/CalcolatriceResDto.java
@@ -0,0 +1,18 @@
+package it.sirfin.calcolatrice21sp.dto;
+
+public class CalcolatriceResDto {
+
+    private double risultato;
+
+    public CalcolatriceResDto() {
+    }
+
+    public double getRisultato() {
+        return risultato;
+    }
+
+    public void setRisultato(double risultato) {
+        this.risultato = risultato;
+    }
+
+}
