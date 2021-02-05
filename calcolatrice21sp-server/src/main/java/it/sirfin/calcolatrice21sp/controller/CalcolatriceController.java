b999ee4e2fa46e33808abc55a98c8a197f7592d7
luca-lezzerini <47611629+luca-lezzerini@users.noreply.github.com>
2021-02-05 10:31:17 +0100

    Aggiornato controller

--- calcolatrice21sp-server/src/main/java/it/sirfin/calcolatrice21sp/controller/CalcolatriceController.java
+++ calcolatrice21sp-server/src/main/java/it/sirfin/calcolatrice21sp/controller/CalcolatriceController.java
@@ -1,9 +1,14 @@
 package it.sirfin.calcolatrice21sp.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  *
  * @author Hp Desktop
  */
+@CrossOrigin("*")
+@RestController
 public class CalcolatriceController {
     
 }
